2abbb276b7a4be78f0999b0453fb890568082c70
patrickjohncyh <patrickjohncyh@icloud.com>
2017-11-13 19:37:27 +0000

    Add .gitignore file and clean up test bench code

--- testbench/testbench.sh
+++ testbench/testbench.sh
@@ -1,33 +1,20 @@
 #!/bin/bash
-
-
-#NUM_TEST_CASES=$(ls -1 testbench/testcases | grep '\.bin$' | wc -l)
-
 shopt -s nullglob
 TEST_CASE_PATH=(testbench/testcases/*.bin)
 GOLDEN_PATH=(testbench/testcases/*.gld)
-Instruction=""
-Author=""
-Golden=""
 
 for ((i=0;i<${#TEST_CASE_PATH[@]};i++))
 	do	
-		#echo ${GOLDEN_PATH[i]}
+		Curr_test_path=${TEST_CASE_PATH[i]}
+		Curr_golden_path=${GOLDEN_PATH[i]}
 		while read Instruction Golden Author;
 		do
 			true
-		done < ${GOLDEN_PATH[i]}
-		$1 ${TEST_CASE_PATH[i]}
+		done < $Curr_golden_path
+		$1 $Curr_test_path
 		Output=$?
 		if [ "$Output" = "$Golden" ]; then Status="Pass"; else Status="Fail"; fi
-		echo "$i,$Instruction,$Status,$Author"
-
+		echo "$i,$Instruction,$Status,$Author,$(basename $Curr_test_path)"
 	done
-	#	echo ${TEST_CASE_PATH[i]}
-	#	$1 ${TEST_CASE_PATH[i]} #to run case
-	#	echo $(($? == 7))
-	
 
-
-
-
+#NUM_TEST_CASES=$(ls -1 testbench/testcases | grep '\.bin$' | wc -l)

ab6d9e8539fc5193ab3124d4679014489dc6ff6c
Kaynelua <luayinghao95@hotmail.com>
2017-12-02 17:58:44 +0000

    test gen for SLT and SLTU

---
+++ testbench/testcase_generator/SLT/SLT_testgen.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstdlib>
+
+
+using namespace std;
+
+
+int main(){
+
+	ofstream outfile;
+	ofstream outfile_gld;
+	ifstream infile;
+
+	int gld 	   = 0;
+	int input_reg1  = 0;
+	string input_data1;
+	int input_reg2 = 0;
+	string input_data2;
+
+
+	string dummyLine;
+	string base = "";
+	string filename;
+
+
+	infile.open("testbench/testcase_generator/SLT/test_data.txt");
+
+	getline(infile, dummyLine);
+	while(infile >> gld >> input_reg1 >> input_data1 >> input_reg2 >> input_data2 >> filename ){
+		outfile.open(("testbench/SLT_tc/" + filename + ".s").c_str());
+		outfile_gld.open(("testbench/SLT_tc/" + filename + ".mips.gld").c_str());
+
+
+/****************************************************************/
+		outfile << ".set noreorder" << endl;
+		outfile << ".set noat" 		<< endl;
+
+		// Set up input_data in input_reg:
+		outfile << "li $" 	 	<< input_reg1 <<  ", 0x" << input_data1 << endl;
+		outfile << "li $" 	 	<< input_reg2  <<  ", 0x" <<  input_data2    << endl;
+		// if reg 1 < reg 2 set $2 to 1:
+		outfile << "slt $2, $" 	<< input_reg1	 << ", $" <<input_reg2 	<<		  endl;
+		
+	
+ 		outfile << "jr   $0" 		<< endl;
+/****************************************************************/
+ 		outfile_gld << "SLT " << gld << " yh";
+
+/****************************************************************/ 		
+
+
+ 		outfile_gld.close();
+		outfile.close();
+	}
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+

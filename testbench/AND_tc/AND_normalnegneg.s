51edafd48e87265a3cc485f66e670f93417bfa1d
Kaynelua <luayinghao95@hotmail.com>
2017-12-02 16:52:45 +0000

    Test gen for And and OR

---
+++ testbench/AND_tc/AND_normalnegneg.s
@@ -0,0 +1,6 @@
+.set noreorder
+.set noat
+li $3, 0x800000F2
+li $2, 0xFFFFFFD6
+and $2, $3, $2
+jr   $0
---
+++ testbench/OR_tc/OR_normalpospos.s
@@ -0,0 +1,6 @@
+.set noreorder
+.set noat
+li $3, 0x7FFFFF1F
+li $2, 0x95
+or $2, $3, $2
+jr   $0

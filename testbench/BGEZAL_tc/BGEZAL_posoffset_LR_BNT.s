6b0b7db750f1e1d195121f557d052c7efc6d9d1f
Kaynelua <luayinghao95@hotmail.com>
2017-12-06 21:28:07 +0000

    Renamed author user ID with Imperial login ID and edited test case for BGEZAL to update LR irregardless of whether branch was taken

--- testbench/BGEZAL_tc/BGEZAL_posoffset_LR_BNT.s
+++ testbench/BGEZAL_tc/BGEZAL_posoffset_LR_BNT.s
@@ -1,13 +1,13 @@
 .set noreorder
 .set noat
 li $2, 0x81234567
-bgezal $2 , branchoffset0000000C
-li $31 , 0x45
+bgezal $2 , branchoffset00000008
+nop
 branchoffset00000004 :
 nop
 branchoffset00000008 :
-li $2, 0x35
+add $2,$0,$31
 branchoffset0000000C :
 nop
 jr   $0
-add $2, $0, $31
+nop
